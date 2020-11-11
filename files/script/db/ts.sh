d672c029fce2290983956650ea6f06eeed954f68
ioannis1 <itambouras@users.noreply.github.com>
2020-11-11 14:49:59 -0500

    pgbouncer_fdw

--- files/script/db/ts.sh
+++ files/script/db/ts.sh
@@ -10,3 +10,13 @@ psql -1Xq -d ts -U postgres -h $HOSTNAME -p $PPORT <<eom
       CREATE EXTENSION IF NOT EXISTS pg_stat_statements ;
 eom
 
+psql -1Xq -d ts -U postgres -h $HOSTNAME -p $PPORT <<eom
+      CREATE EXTENSION IF NOT EXISTS dblink;
+      CREATE EXTENSION IF NOT EXISTS pgbouncer_fdw;
+
+      DROP SERVER IF EXISTS pgbouncer CASCADE;
+      CREATE SERVER pgbouncer FOREIGN DATA WRAPPER dblink_fdw OPTIONS (host 'localhost',
+                                                                 port '6432',
+                                                                 dbname 'pgbouncer');
+      CREATE USER MAPPING FOR prometheus SERVER pgbouncer OPTIONS (user 'postgres', password 'silver');
+eom
