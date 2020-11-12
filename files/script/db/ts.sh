e527f56058562a46b883331be4fefc759cc4d8ba
ioannis1 <itambouras@users.noreply.github.com>
2020-11-11 23:00:22 -0500

    pgbouncer_fdw

--- files/script/db/ts.sh
+++ files/script/db/ts.sh
@@ -1,5 +1,6 @@
 HOSTNAME=$1
 PPORT=$2
+PASSWD=$3
 MONITOR_USER=prometheus 
 
 psql -Xq -d postgres  -U postgres  -h $HOSTNAME  -p $PPORT  <<eom
@@ -11,7 +12,7 @@ psql -1Xq -d ts -U postgres -h $HOSTNAME -p $PPORT <<eom
       CREATE EXTENSION IF NOT EXISTS pg_stat_statements ;
 eom
 
-psql -1Xq -d ts -U postgres -h $HOSTNAME -p $PPORT <<eom
+psql -1Xq -d ts -U postgres -h $HOSTNAME -p $PPORT -v passwd="'$PASSWD'" -v muser=${MONITOR_USER} <<eom
       CREATE EXTENSION IF NOT EXISTS dblink;
       CREATE EXTENSION IF NOT EXISTS pgbouncer_fdw;
 
@@ -19,20 +20,21 @@ psql -1Xq -d ts -U postgres -h $HOSTNAME -p $PPORT <<eom
       CREATE SERVER pgbouncer FOREIGN DATA WRAPPER dblink_fdw OPTIONS (host 'localhost',
                                                                  port '6543',
                                                                  dbname 'pgbouncer');
-      CREATE USER MAPPING FOR $MONITOR_USER  SERVER pgbouncer OPTIONS (user 'postgres', password 'secret');
+      CREATE USER MAPPING FOR :muser  SERVER pgbouncer OPTIONS (user 'postgres', password :passwd );
 eom
-psql -1Xq -d ts -U postgres -h $HOSTNAME -p $PPORT <<eom
+
+psql -1Xq -d ts -U postgres -h $HOSTNAME -p $PPORT -v muser=${MONITOR_USER} <<eom
        GRANT USAGE ON FOREIGN SERVER pgbouncer TO $MONITOR_USER ;
 
-       GRANT SELECT ON pgbouncer_clients      TO  $MONITOR_USER ;
-       GRANT SELECT ON pgbouncer_config       TO  $MONITOR_USER ;
-       GRANT SELECT ON pgbouncer_databases    TO  $MONITOR_USER ;
-       GRANT SELECT ON pgbouncer_dns_hosts    TO  $MONITOR_USER ;
-       GRANT SELECT ON pgbouncer_dns_zones    TO  $MONITOR_USER ;
-       GRANT SELECT ON pgbouncer_lists        TO  $MONITOR_USER ;
-       GRANT SELECT ON pgbouncer_pools        TO  $MONITOR_USER ;
-       GRANT SELECT ON pgbouncer_servers      TO  $MONITOR_USER ;
-       GRANT SELECT ON pgbouncer_sockets      TO  $MONITOR_USER ;
-       GRANT SELECT ON pgbouncer_stats        TO  $MONITOR_USER ;
-       GRANT SELECT ON pgbouncer_users        TO  $MONITOR_USER ;
+       GRANT SELECT ON pgbouncer_clients      TO  :muser ;
+       GRANT SELECT ON pgbouncer_config       TO  :muser ;
+       GRANT SELECT ON pgbouncer_databases    TO  :muser ;
+       GRANT SELECT ON pgbouncer_dns_hosts    TO  :muser ;
+       GRANT SELECT ON pgbouncer_dns_zones    TO  :muser ;
+       GRANT SELECT ON pgbouncer_lists        TO  :muser ;
+       GRANT SELECT ON pgbouncer_pools        TO  :muser ;
+       GRANT SELECT ON pgbouncer_servers      TO  :muser ;
+       GRANT SELECT ON pgbouncer_sockets      TO  :muser ;
+       GRANT SELECT ON pgbouncer_stats        TO  :muser ;
+       GRANT SELECT ON pgbouncer_users        TO  :muser ;
 eom
